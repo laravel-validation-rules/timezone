12ac6f802800443f5d216e42d41e1b135ec5f417
Scott Wilcox <scott@dor.ky>
2017-09-06 18:21:31 +0100

    PSR2 run through

--- src/Timezone.php
+++ src/Timezone.php
@@ -20,7 +20,7 @@ class Timezone implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
+     * @param  string $attribute
      * @param  mixed  $value
      * @return bool
      */
