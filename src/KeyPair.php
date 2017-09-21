26fd878a043e4fa21d0570cdf79f53ae41ba4d3f
Scott Wilcox <scott@dor.ky>
2017-09-21 17:34:59 +0100

    Initial skeleton

---
+++ src/KeyPair.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SSX\SSH;
+
+class KeyPair
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+}
---
+++ tests/KeypairTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SSX\SSH\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class KeypairTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function testItCanGenerateAKeyPair()
+    {
+        $this->assertTrue(false);
+    }
+
+
+}
